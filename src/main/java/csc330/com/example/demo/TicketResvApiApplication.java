1a2373921bd56c69fa6c7835d186fbfae1ac7df3
Enrique Barrios <EnriqueB5310@gmail.com>
2023-10-01 16:00:20 -0400

    Initial commit

---
+++ src/main/java/csc330/com/example/demo/TicketResvApiApplication.java
@@ -0,0 +1,13 @@
+package csc330.com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketResvApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketResvApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/csc330/com/example/demo/TicketResvApiApplicationTests.java
@@ -0,0 +1,13 @@
+package csc330.com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketResvApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
